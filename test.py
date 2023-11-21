844d822c02c585b590b69d5fe26b400c52df73a9
jjyw726 <135066357+jjyw726@users.noreply.github.com>
2023-11-21 15:25:40 -0800

    Add file to output list of test

---
+++ test.py
@@ -0,0 +1,17 @@
+def identify_tests (object):
+    All_entry = []
+    for i in range(len(object)):
+        Test_list = [str(i)]
+        if object[i].chestpain > 2:
+            Test_list.append(", electrocardiogram (ECG)")
+        if object[i].cholestrol > 240:
+            Test_list.append(", lipid profile (blood test)")
+        if object[i].maxheartrate > (220-object[i].age):
+            Test_list.append(", electrocardiogram (ECG)")
+        if object[i].plasma_glucose > 2:
+            Test_list.append(", oral glucose tolerance test (OGTT")
+    
+
+    All_entry.append(Test_list)
+        
+
