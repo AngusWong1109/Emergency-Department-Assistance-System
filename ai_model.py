f7efed12ed69ebed69f03dd17069d41e8c861bc4
jla690 <jason.l.i@hotmail.com>
2023-12-03 16:37:06 -0800

    add more comments

--- ai_model.py
+++ ai_model.py
@@ -12,21 +12,24 @@ from tensorflow.keras import optimizers
 from tensorflow.keras.layers import Dropout
 
 class ai_model:
+    # Initialize model
     def __init__(self):
         data = genfromtxt('patient_priority.csv', delimiter=',',
                     skip_header=1, dtype=None, autostrip=True, encoding=None, missing_values="", filling_values=0)
         data = np.asarray(data.tolist())
 
-        # Parse data
+        # Parse data and separate into x and y
         self.train_size = 4000
         self.train_x = data[0:self.train_size]
         self.train_x = self.train_x[:, 1:-1]
         self.train_y = data[0:self.train_size]
         self.train_y = self.train_y[:, -1]
+        # Check if model exists, if not create it
         if os.path.isfile('model.h5'):
             self.model = tensorflow.keras.models.load_model('model.h5')
         else:
             self.model = self.createModel()
+        # Create list of patients
         self.listOfNumpyPatients = []
         self.listOfPatientObjects = []
 
@@ -35,6 +38,7 @@ class ai_model:
         model = Sequential()
         dropoutConstant = 0.1
         initializer = initializers.RandomNormal(mean=0., stddev=0.01)
+        # Add layers
         model.add(Dense(512, input_shape=shape(self.train_x[0]), activation='relu',
                     kernel_regularizer='l2', kernel_initializer=initializer))
         model.add(BatchNormalization())
@@ -48,12 +52,16 @@ class ai_model:
         model.add(Dense(64, activation='relu', kernel_regularizer='l2'))
         model.add(BatchNormalization())
         model.add(Dropout(dropoutConstant))
+        # Output layer, 4 classes so use softmax
         model.add(Dense(4, activation='softmax', kernel_regularizer='l2'))
+        # Compile model
         model.compile(loss='sparse_categorical_crossentropy',
                         optimizer=tensorflow.optimizers.Adam(learning_rate=0.0002), metrics=['accuracy'])
+        # Fit model
         model.fit(self.train_x, self.train_y, epochs=300,
                     batch_size=256)
         _, accuracy = model.evaluate(self.train_x, self.train_y)
+        # Save model
         model.save('model.h5')
         return model
 
@@ -69,7 +77,9 @@ class ai_model:
     # Predict on data, use this once all the patients have been inserted
     def predict_on_data(self):
         prediction = self.model.predict(np.asarray(self.listOfNumpyPatients))
+        # Get the highest probability
         prediction = np.argmax(prediction, axis=1)
+        # Update the triage value of the patient
         for x in range(len(self.listOfPatientObjects)):
             self.listOfPatientObjects[x].triage = prediction[x]
         return prediction
